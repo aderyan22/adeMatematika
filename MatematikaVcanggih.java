5b8c0a9b1a90e021d241361febb443151718dd66
aderyan22 <adepermadhie29@gmail.com>
2024-12-04 12:44:42 +0700

    Add files via upload

---
+++ MatematikaVcanggih.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class MatematikaVcanggih extends MatematikaV {
+   void modulus(double a ,double b , double c){
+        double hasil = a % b % c;
+        System.out.println("Hasil Modulus: " + hasil);
+    }
+    void modulus(int a ,int b , int c){
+        int hasil = a % b % c;
+        System.out.println("Hasil Modulus: " + hasil);
+    }
+    void modulus(int a ,int b){
+        int hasil = a % b;
+        System.out.println("Hasil Modulus: " + hasil);
+    }
+    void modulus(double a ,double b){
+        double hasil = a % b ;
+        System.out.println("Hasil Modulus: " + hasil);
+    }
+}
